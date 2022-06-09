c0b39c288f8ec28ea42070232ba2407fe8e854a0
Lusindiso Ntanjana <ntanjanalusindiso8@gmail.com>
2022-06-09 11:13:44 +0200

    Update CountryList.js

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -24,7 +24,7 @@ const CountryList = props => {
 
   const renderList = () =>{
     if(props.countries){
-      props.countries
+      return props.countries
         .filter(item =>
           item.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()),
         )
